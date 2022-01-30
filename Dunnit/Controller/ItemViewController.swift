d45679b4553958e325950b886e8dc53a812896ef
Aakarshit <aakarshitjaswal@gmail.com>
2022-01-30 15:45:11 +0530

    Search backspace bug fix, Everything working

--- Dunnit/Controller/ItemViewController.swift
+++ Dunnit/Controller/ItemViewController.swift
@@ -26,18 +26,18 @@ class ItemViewController: UITableViewController {
     var items: Results<Item>?
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       items?.count ?? 0
+        items?.count ?? 0
     }
     
     //MARK: TableView Data Source
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath)
         
-            cell.textLabel?.text = items?[indexPath.row].title ?? "No items added yet"
-            
-            cell.accessoryType = items?[indexPath.row].done ?? false ? .checkmark : .none
-    
+        cell.textLabel?.text = items?[indexPath.row].title ?? "No items added yet"
+        
+        cell.accessoryType = items?[indexPath.row].done ?? false ? .checkmark : .none
+        
         return cell
     }
     
@@ -74,12 +74,30 @@ class ItemViewController: UITableViewController {
     
     
     //MARK: TableView Data Manipulation
-
+    
     func loadData() {
         items = selectedCategory.items.sorted(byKeyPath: "title", ascending: true)
         tableView.reloadData()
         
     }
+    
+    //MARK: TableView Delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let selectedItem = items?[indexPath.row] {
+            do {
+                try realm.write {
+                    selectedItem.done = !selectedItem.done
+                }
+            } catch {
+                print(error)
+            }
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        tableView.reloadData()
+    }
+    
     
 }
 
@@ -89,25 +107,30 @@ extension ItemViewController: UISearchBarDelegate {
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         if searchBar.text!.count > 0 {
-        items = items?.filter("title CONTAINS[cd] %@", searchBar.text!).sorted(byKeyPath: "dateAdded", ascending: true)
+            items = items?.filter("title CONTAINS[cd] %@", searchBar.text!).sorted(byKeyPath: "dateAdded", ascending: true)
         } else {
+            
             loadData()
         }
+        
         tableView.reloadData()
         
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         if searchBar.text?.count == 0 {
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
             loadData()
         } else {
-            items = items?.filter("title CONTAINS[cd] %@", searchBar.text!).sorted(byKeyPath: "dateAdded", ascending: true)
-            tableView.reloadData()
+            DispatchQueue.main.async {
+                self.items = self.realm.objects(Item.self).filter("title CONTAINS[cd] %@", searchBar.text!).sorted(byKeyPath: "dateAdded", ascending: true)
+                self.tableView.reloadData()
+            }
         }
     }
-
 }
-
 
 
 
