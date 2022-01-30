74b5833b7dd5e91801e809e42ef28df34ffea04e
Aakarshit <aakarshitjaswal@gmail.com>
2022-01-30 12:26:50 +0530

    Set up add new Item, Everything working as expected

--- Dunnit/Controller/ItemViewController.swift
+++ Dunnit/Controller/ItemViewController.swift
@@ -11,26 +11,85 @@ import RealmSwift
 
 class ItemViewController: UITableViewController {
     
+    let realm = try! Realm()
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
-    var selectedCategory = Category()
+    var selectedCategory = Category() {
+        didSet {
+            loadData()
+        }
+    }
+    
+    var items: Results<Item>?
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       items?.count ?? 0
+    }
+    
+    //MARK: TableView Data Source
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath)
+        
+            cell.textLabel?.text = items?[indexPath.row].title ?? "No items added yet"
+            
+            cell.accessoryType = items?[indexPath.row].done ?? false ? .checkmark : .none
+    
+        return cell
+    }
+    
+    @IBAction func addItemPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add a new Dunnit", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add", style: .default) { action in
+            let newItem = Item()
+            newItem.title = textField.text!
+            do {
+                try self.realm.write {
+                    self.selectedCategory.items.append(newItem)
+                }
+            } catch {
+                print(error)
+            }
+            
+            self.loadData()
+            
+        }
+        
+        alert.addAction(action)
+        
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Type here"
+            textField = alertTextField
+        }
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
+    //MARK: TableView Data Manipulation
+
+    func loadData() {
+        items = selectedCategory.items.sorted(byKeyPath: "title", ascending: true)
+        tableView.reloadData()
+        
+    }
+    
+    
+    
+    
+    
+    
     
     
     
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
